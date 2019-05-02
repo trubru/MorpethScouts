f8a0858e9c90dcb2010d3606ef4bebf2b86b2323
trubru <stuartedwardholmes@outlook.com>
2019-05-02 10:55:42 +0100

    tidied up scripts files and mobile menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,35 +1,35 @@
-    function ConfigureMenuItems() {
-        var classname = document.querySelectorAll(".MenuItem");
+ConfigureShowHideMenuButtons();
+ConfigureMenuItems();   
 
-        for (var i = 0; i < classname.length; i++) {
-            classname[i].addEventListener('click', function(event) {
-                HideMenu();
-            });
-        }
-    }
-
-    function ConfigureShowHideMenuButtons() {
-        document.getElementById("ShowMenu").addEventListener("click", function(event){ 
-            ShowMenu();
-            document.getElementById("MainMenu").focus();
-            event.preventDefault();
-        });
-
-        document.getElementById("HideMenu").addEventListener("click", function(event){ 
+function ConfigureMenuItems() {
+    var classname = document.querySelectorAll(".MenuItem");
+    for (var i = 0; i < classname.length; i++) {
+        classname[i].addEventListener('click', function(event) {
             HideMenu();
-            document.getElementById("ShowMenu").focus();                
-            event.preventDefault();
         });
-    }            
-
-    function HideMenu() {
-        console.log("Hide Menu");
-        document.getElementById("MenuWrapper").classList.remove("ShowTheMenu");
-        document.getElementById("MenuWrapper").classList.add("HideTheMenu");
     }
+}
 
-    function ShowMenu() {
-        console.log("Show Menu");
-        document.getElementById("MenuWrapper").classList.add("ShowTheMenu");
-        document.getElementById("MenuWrapper").classList.remove("HideTheMenu");
-    }
+function ConfigureShowHideMenuButtons() {
+    document.getElementById("ShowMenu").addEventListener("click", function(event){ 
+        ShowMenu();
+        document.getElementById("MainMenu").focus();
+        event.preventDefault();
+    });
+
+    document.getElementById("HideMenu").addEventListener("click", function(event){ 
+        HideMenu();
+        document.getElementById("ShowMenu").focus();                
+        event.preventDefault();
+    });
+}            
+
+function HideMenu() {
+    document.getElementById("MenuWrapper").classList.remove("ShowTheMenu");
+    document.getElementById("MenuWrapper").classList.add("HideTheMenu");
+}
+
+function ShowMenu() {
+    document.getElementById("MenuWrapper").classList.add("ShowTheMenu");
+    document.getElementById("MenuWrapper").classList.remove("HideTheMenu");
+}
